8990b77655c399e8ebccf36660e79a104e3c4a8c
Shafique Dassu <shafd1231@gmail.com>
2023-08-21 17:10:41 +0100

    Initial Commit

---
+++ JointWellnessApp/OnboardingCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  JointWellnessApp
+//
+//  Created by shafique dassu on 21/08/2023.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
