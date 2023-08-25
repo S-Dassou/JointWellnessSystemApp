531b4266f65f70246c60b1cdc44f4b6bbc72da94
Shafique Dassu <shafd1231@gmail.com>
2023-08-25 14:10:47 +0100

    Onboarding views with collection views implemented Estimate size set to 0 Lottie imported Lottie files added

--- JointWellnessApp/OnboardingCollectionViewCell.swift
+++ JointWellnessApp/OnboardingCollectionViewCell.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import Lottie
+
 
 class OnboardingCollectionViewCell: UICollectionViewCell {
-
+    
+//    var identifier: String
+    @IBOutlet weak var animationContainer: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    static let identifier = "OnboardingCollectionViewCell"
+    
+    var animation = LottieAnimationView()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func configureCell(page: Page) {
+        animation = LottieAnimationView(name: page.animationName)
+        animation.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height * 0.8)
+        
+        animation.animationSpeed = 1
+        animation.loopMode = .loop
+        animation.play()
+        
+        animationContainer.addSubview(animation)
+        
+        self.titleLabel.text = page.title
+        self.descriptionLabel.text = page.description
+    }
 }
--- JointWellnessApp/ViewController.swift
+++ JointWellnessApp/ViewController.swift
@@ -7,13 +7,73 @@
 
 import UIKit
 
+struct Page {
+    let animationName: String
+    let title: String
+    let description: String
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var onboardingCollectionView: UICollectionView!
+    @IBOutlet weak var onboardingPageControl: UIPageControl!
+    @IBOutlet weak var onboardingSkipButton: UIButton!
+    
+//    var titles: [String] = ["title 1", "title 2", "title 3"]
+//    var descriptions: [String] = ["desc 1", "desc 4", "desc 3"]
+    
+    
+    let pages: [Page] = [
+        Page(animationName: "strong", title: "Improve Strength", description: "This is our Recipe"),
+        Page(animationName: "stretch", title: "Get Flexy", description: "A Step by Step Guide"),
+        Page(animationName: "dance", title: "Be Free", description: "Our Simple Formula to Freedom")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        onboardingSkipButton.layer.cornerRadius = 8
+        onboardingCollectionView.dataSource = self
+        onboardingCollectionView.delegate = self
+        onboardingCollectionView.register(UINib(nibName: "OnboardingCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "OnboardingCollectionViewCell")
+        
+        self.onboardingPageControl.numberOfPages = pages.count
     }
 
 
+    @IBAction func pageChange(_ sender: Any) {
+        let pc = sender as! UIPageControl
+        
+        onboardingCollectionView.scrollToItem(at: IndexPath(item: pc.currentPage, section: 0), at: .centeredHorizontally, animated: true)
+    }
+    
+    
+    @IBAction func onboardingButtonTapped(_ sender: Any) {
+        print("move to other VC")
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = onboardingCollectionView.dequeueReusableCell(withReuseIdentifier: "OnboardingCollectionViewCell", for: indexPath) as! OnboardingCollectionViewCell
+        
+        cell.configureCell(page: pages[indexPath.item])
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: self.onboardingCollectionView.frame.width, height: self.onboardingCollectionView.frame.height)
+    }
+    
+    //update UI Page Control
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        onboardingPageControl.currentPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
+    }
 }
 
