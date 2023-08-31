e31f92d1d785de210b5ef8db15325dd3a73789e5
Shafique Dassu <shafd1231@gmail.com>
2023-08-31 23:43:38 +0100

    Added table view to home page vc

--- JointWellnessApp/HomeViewController.swift
+++ JointWellnessApp/HomeViewController.swift
@@ -9,21 +9,27 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var homeTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      
+    }
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
-    */
+    
+    
+}
 
+extension HomeViewController: UITableViewDelegate {
+    
 }
