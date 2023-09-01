7da3b4a69dcdac39e678e77477411c028c28c3a6
Shafique Dassu <shafd1231@gmail.com>
2023-09-01 23:14:02 +0100

    created initial category struct model for table view (in HomeVC) configured table view (delegate + data source) Added table view category image assets

---
+++ JointWellnessApp/HomeTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTableViewCell.swift
+//  JointWellnessApp
+//
+//  Created by shafique dassu on 01/09/2023.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
--- JointWellnessApp/HomeViewController.swift
+++ JointWellnessApp/HomeViewController.swift
@@ -7,29 +7,55 @@
 
 import UIKit
 
+struct Category {
+    let catImage: UIImage
+    let catName: String
+}
+
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var homeTableView: UITableView!
     
+    let categories: [Category] = [
+    Category(catImage: UIImage(named: "hips")!, catName: "Hips"),
+    Category(catImage: UIImage(named: "atWork")!, catName: "At Work"),
+    Category(catImage: UIImage(named: "shoulder")!, catName: "Shoulder Mobility"),
+    Category(catImage: UIImage(named: "clock")!, catName: "5 Minute Fix")
+    ]
+    
+    var categoryImages: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        homeTableView.estimatedRowHeight = 80
+        homeTableView.rowHeight = UITableView.automaticDimension
+        homeTableView.tableFooterView = UIView()
+        
+        homeTableView.dataSource = self
+        homeTableView.delegate = self
       
     }
 }
 
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return categories.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let category = categories[indexPath.row]
+        let cell = homeTableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as! HomeTableViewCell
+        cell.selectionStyle = .none
+        cell.categoryLabel.text = category.catName
+        cell.categoryImage.image = category.catImage
+        return cell
     }
     
     
 }
 
 extension HomeViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
 }
