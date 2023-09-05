42b43ad894afb5afa4a4d494a357def5aca50cb8
Shafique Dassu <shafd1231@gmail.com>
2023-09-05 18:46:51 +0100

    Modelled stretch category, subcategory and exercises

---
+++ JointWellnessApp/Models/StretchCategoryModel.swift
@@ -0,0 +1,67 @@
+//
+//  StretchCategory.swift
+//  JointWellnessApp
+//
+//  Created by shafique dassu on 04/09/2023.
+//
+
+import Foundation
+import UIKit
+
+struct StretchCategory {
+    var name: String
+    var catImage: String?
+   // var routines: [StretchRoutine]
+}
+
+struct StretchRoutine {
+    var title: String
+    var description: String
+    var duration: Int
+    var category: StretchCategory
+    var steps: [StretchExercise]
+}
+
+struct StretchExercise {
+    var name: String
+    var description: String
+    var time: Int
+    var lottieAnimation: String?
+}
+
+let categories: [StretchCategory] = []
+let routines: [StretchRoutine] = []
+let exercises: [StretchExercise] = []
+
+let armRoutine1Exercises: [StretchExercise] = [
+StretchExercise(name: "tricep stretch 1", description: "lift arm 1", time: 1),
+StretchExercise(name: "tricep stretch 2", description: "lift arm 2", time: 1),
+StretchExercise(name: "tricep stretch 3", description: "lift arm 3", time: 1)
+]
+
+let category1 = StretchCategory(name: "Arms")
+let armRoutine1 = StretchRoutine(title: "tricep", description: "10 minute stretch", duration: 10, category: category1, steps: armRoutine1Exercises)
+
+
+
+
+//let armCategory = StretchCategory(name: "Arms", catImage: "shoulder", routines: [
+//StretchRoutine(title: "Easy", description: "5 min routine for beginners to stretch your arms", duration: 5, steps: [
+//    StretchExercise(name: "Arm Stretch 1", description: "gently push your fingers back", time: 1),
+//    StretchExercise(name: "Arm Stretch 2", description: "Move your arms above your head", time: 1),
+//    StretchExercise(name: "Arm Stretch 3", description: "Stretch your shoulders", time: 1),
+//    StretchExercise(name: "Arm Stretch 4", description: "Stretch your tricep", time: 1)
+//    ]),
+//StretchRoutine(title: "Medium", description: "5 min routine for intermediate to stretch your arms", duration: 5, steps: [
+//    StretchExercise(name: "Arm Stretch 1", description: "gently push your fingers back", time: 1),
+//    StretchExercise(name: "Arm Stretch 2", description: "Move your arms above your head", time: 1),
+//    StretchExercise(name: "Arm Stretch 3", description: "Stretch your shoulders", time: 1),
+//    StretchExercise(name: "Arm Stretch 4", description: "Stretch your tricep", time: 1)
+//]),
+//StretchRoutine(title: "Hard", description: "5 min routine for experts to stretch your arms", duration: 5, steps: [
+//    StretchExercise(name: "Arm Stretch 1", description: "gently push your fingers back", time: 1),
+//    StretchExercise(name: "Arm Stretch 2", description: "Move your arms above your head", time: 1),
+//    StretchExercise(name: "Arm Stretch 3", description: "Stretch your shoulders", time: 1),
+//    StretchExercise(name: "Arm Stretch 4", description: "Stretch your tricep", time: 1)
+//])
+//])
